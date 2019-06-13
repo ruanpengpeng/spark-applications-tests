b00ed36cd5e3426bb607ba9c769658fdf5a80740
ruanpp <15757101516@163.com>
2019-06-13 17:56:49 +0800

    original version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spark-applications-tests
 自己练手的spark项目
+包括从mysql中的json获取解析，并组装成sql语句使用
+sparksql进行处理,最后将数据保存到hbase中
---
+++ src/main/java/rpp/data/LZOSEQUNCEFILE.scala
@@ -0,0 +1,7 @@
+package rpp.data
+
+object LZOSEQUNCEFILE {
+  def main(args: Array[String]): Unit = {
+    println("hello 2019/6/13")
+  }
+}
